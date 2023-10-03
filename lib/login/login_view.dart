cae275daa442247c5d12593f3cfc5dd849fd2526
Bayu Nugroho <nugrohob825@gmail.com>
2023-10-03 20:51:50 +0700

    fix: login page

--- lib/login/login_view.dart
+++ lib/login/login_view.dart
@@ -15,93 +15,114 @@ class LoginView extends StatelessWidget {
         create: (context) => LoginViewModel(),
         builder: (context, child) {
           return Scaffold(
-            // appBar: AppBar(
-            //   title: const Text("Aplikasi October"),
-            //   centerTitle: true,
-            // ),
             body: Consumer<LoginViewModel>(builder: (context, provider, child) {
-              return SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 32.0, vertical: 16),
-                  child: Center(
-                    child: Form(
-                      key: provider.formKey,
-                      child: Column(
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 32,
+                  vertical: 32,
+                ),
+                child: Form(
+                  key: provider.formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text("Welcome,\nLog in now to continue"),
+                      Image.asset("assets/images/login-icon.png"),
+                      TextFormComponent(
+                        name: "Email",
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Email tidak boleh kosong";
+                          } else if (!provider.emailValidation(value)) {
+                            return "Format email salah";
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      TextFormComponent(
+                        name: "Password",
+                        obscureText: !provider.showPassword,
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            provider.toggleVisibility();
+                          },
+                          child: Icon(
+                            provider.showPassword
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Password tidak boleh kosong";
+                          }
+                          return null;
+                        },
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
                         children: [
-                          SizedBox(
-                            height: queryData.size.height * 0.08,
-                          ),
-                          Image.asset("assets/images/dashboard-icon.png"),
-                          SizedBox(
-                            height: queryData.size.height * 0.05,
-                          ),
-                          const Text(
-                            "Wellcome!",
-                            style: TextStyle(
-                                fontSize: 32, fontWeight: FontWeight.w600),
-                          ),
-                          SizedBox(
-                            height: queryData.size.height * 0.02,
-                          ),
-                          TextFormComponent(
-                            name: "Email",
-                            keyboardType: TextInputType.emailAddress,
-                            validator: (value) {
-                              if (value!.isEmpty) {
-                                return "Email tidak boleh kosong";
-                              } else if (!provider.emailValidation(value)) {
-                                return "Format email salah";
-                              }
-                              return null;
-                            },
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          TextFormComponent(
-                            name: "Password",
-                            obscureText: !provider.showPassword,
-                            suffixIcon: GestureDetector(
-                              onTap: () {
-                                provider.toggleVisibility();
-                              },
-                              child: Icon(
-                                provider.showPassword
-                                    ? Icons.visibility
-                                    : Icons.visibility_off,
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              top: 8.0,
+                            ),
+                            child: GestureDetector(
+                              onTap: () {},
+                              child: const Text(
+                                "Forgot Password?",
+                                style: TextStyle(
+                                  color: Colors.blueAccent,
+                                ),
                               ),
                             ),
-                            validator: (value) {
-                              if (value!.isEmpty) {
-                                return "Password tidak boleh kosong";
-                              }
-                              return null;
-                            },
                           ),
-                          SizedBox(
-                            height: queryData.size.height * 0.06,
-                          ),
-                          SizedBox(
-                            width: queryData.size.width * 0.9,
-                            height: queryData.size.height * 0.07,
-                            child: ElevatedButton(
-                                onPressed: () {
-                                  if (provider.formKey.currentState!
-                                      .validate()) {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) =>
-                                              const UserProfile(),
-                                        ));
-                                  }
-                                },
-                                child: const Text("Login")),
-                          )
                         ],
                       ),
-                    ),
+                      SizedBox(
+                        height: queryData.size.height * 0.04,
+                      ),
+                      SizedBox(
+                        width: queryData.size.width * 0.9,
+                        height: queryData.size.height * 0.07,
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            )),
+                            onPressed: () {
+                              if (provider.formKey.currentState!.validate()) {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const UserProfile(),
+                                    ));
+                              }
+                            },
+                            child: const Text(
+                              "Login",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 14,
+                              ),
+                            )),
+                      ),
+                      const Spacer(),
+                      const Center(
+                        child: Text.rich(
+                          TextSpan(text: "Don't have account?", children: [
+                            TextSpan(
+                                text: " Register",
+                                style: TextStyle(
+                                  color: Colors.blueAccent,
+                                ))
+                          ]),
+                        ),
+                      )
+                    ],
                   ),
                 ),
               );
